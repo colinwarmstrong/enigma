5ff07d810ba0c4a51ebd8ed3bac10fe6589b9b6a
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-12 15:31:40 -0600

    Added decrypt method and created enigma class

--- runner.rb
+++ runner.rb
@@ -1,6 +1,8 @@
 require './lib/key_generator'
 require './lib/offsets_calculator.rb'
 require './lib/encryptor.rb'
+require './lib/decryptor.rb'
+require './lib/enigma.rb'
 require 'pry'
 
 key_generator = KeyGenerator.new
@@ -12,5 +14,7 @@ rotations = key_generator.define_rotations(key)
 offsets_calculator =  OffsetsCalculator.new
 offsets = offsets_calculator.define_offsets
 
-encryptor = Encryptor.new("Hello1!", [2, 3, 4, 5], [2, 3, 4, 5])
-p encryptor.encrypt
+e = Enigma.new("Hello1!", [2, 3, 4, 5], [2, 3, 4, 5])
+encrypted_message = e.encrypt
+p encrypted_message
+p e.decrypt(encrypted_message)
