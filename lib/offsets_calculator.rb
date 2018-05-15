ca50b5438dbb7aa1be936bf5e38d958fe57fd5b0
ClaireBvs <clairembeauvais@gmail.com>
2018-05-15 13:39:39 -0600

    refactor test and code for OffsetsCalculator class

--- lib/offsets_calculator.rb
+++ lib/offsets_calculator.rb
@@ -1,42 +1,20 @@
 require 'date'
 
 class OffsetsCalculator
-
   attr_reader :date
 
-  def initialize(date = Date.today)
-    @date = date
+  def initialize(date = Date.today.strftime("%d%m%y"))
+    @date = date.to_i
   end
 
-  def format_date_to_numeric
-    date_string = @date.to_s
-    day = date_string[8..9]
-    month = date_string[5..6]
-    year = date_string[2..3]
-    date_string_formatted = day + month + year
-    numeric_date = date_string_formatted.to_i
-    return numeric_date
-  end
-
-  def square_numeric_date(numeric_date)
-    return numeric_date ** 2
-  end
-
-  def find_last_four_digits_of_squared_numeric(squared_numeric)
-    last_four_digits = []
-    squared_numeric_array = squared_numeric.to_s.split("")
-    last_four_digits.push(squared_numeric_array[-4..-1])
-    last_four_digits.flatten!
-    return last_four_digits
+  def square_numeric_date(date)
+    return date ** 2
   end
 
   def define_offsets
-    numeric_date = format_date_to_numeric
-    numeric_date_squared = square_numeric_date(numeric_date)
-    last_four_digits = find_last_four_digits_of_squared_numeric(numeric_date_squared)
-    offsets = last_four_digits.map do |digit|
-      digit.to_i
-    end
+    square_num = square_numeric_date(@date)
+    squared_numeric_array = square_num.digits.reverse
+    offsets = squared_numeric_array[-4..-1]
     return offsets
   end
 
--- test/offsets_calculator_test.rb
+++ test/offsets_calculator_test.rb
@@ -10,32 +10,28 @@ class OffsetsCalculatorTest < Minitest::Test
     assert_instance_of OffsetsCalculator, offsets_calculator
   end
 
-  def test_format_date_to_numeric_returns_date_in_correct_form
+  def test_correct_date_format
     offsets_calculator = OffsetsCalculator.new
 
-    date_in_numeric_form = offsets_calculator.format_date_to_numeric
-    assert_equal 6, date_in_numeric_form.digits.length
-    assert_instance_of Integer, date_in_numeric_form
+    assert_instance_of Integer, offsets_calculator.date
+    assert_equal 6, offsets_calculator.date.digits.length
   end
 
   def test_it_can_square_numeric_date
     offsets_calculator = OffsetsCalculator.new
-    numeric_date = 150218
+    numeric_date1 = 150_218
+    numeric_date2 = 140_706
 
-    assert_equal 22565447524, offsets_calculator.square_numeric_date(numeric_date)
+    assert_equal 22_565_447_524, offsets_calculator.square_numeric_date(numeric_date1)
+    assert_equal 19_798_178_436, offsets_calculator.square_numeric_date(numeric_date2)
   end
 
-  def test_if_we_can_find_last_four_digits_of_numeric
-    offsets_calculator = OffsetsCalculator.new
+  def test_define_offsets
+    offsets_calculator1 = OffsetsCalculator.new(150218)
+    offsets_calculator2 = OffsetsCalculator.new(140706)
 
-    assert_equal ['2', '3', '4', '1'], offsets_calculator.find_last_four_digits_of_squared_numeric(92341)
-  end
-
-  def test_if_we_return_correct_offsets
-    offsets_calculator = OffsetsCalculator.new
-
-    assert_instance_of Array, offsets_calculator.define_offsets
-    assert_equal 4, offsets_calculator.define_offsets.count
+    assert_equal [7, 5, 2, 4], offsets_calculator1.define_offsets
+    assert_equal [8, 4, 3, 6], offsets_calculator2.define_offsets
   end
 
 end
